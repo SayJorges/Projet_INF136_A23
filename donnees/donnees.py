6eac2a0ae94139129327c97493944f9742176b0a
hemer <hemery.baptiste@gmail.com>
2023-10-31 19:23:08 -0400

    gg

--- donnees/donnees.py
+++ donnees/donnees.py
@@ -1,15 +1,22 @@
 from constantes import *
+from references import *
+from images.image import *
 
 
 def charger_references():
     lettre = REFERENCE_LETTRES
     chiffre = REFERENCE_CHIFFRES
+
     # cree le dictionnaire
     dictionnaire_caractere = {}
+
+    # charger une image
+    charger_jpeg()
     for nombre in chiffre:
-        dictionnaire_caractere.append(nombre)
+        dictionnaire_caractere[nombre] = [charger_jpeg]
+
     for element in lettre:
-        dictionnaire_caractere.append(element)
+        dictionnaire_caractere[element] = [charger_jpeg]
 
     return dictionnaire_caractere
 
