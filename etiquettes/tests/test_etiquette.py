bd994a62190c42160c0ab7d1f2a34a9c213ce7f2
Sayjorges <georges.sayegh.1@ens.etsmtl.ca>
2023-11-06 20:57:37 -0500

    sayjorges

--- etiquettes/tests/test_etiquette.py
+++ etiquettes/tests/test_etiquette.py
@@ -8,19 +8,19 @@ from etiquettes.etiquette import *
 # Constantes internes
 CHEMIN_ETIQUETTE_TEST = r'..\..\donnees\etiquettes\1.jpg'
 
+
+def test_visuel_etiquette_decouper():
+   """
+   Test visuel de la fonction de découpage.
+   """
+
+   # Charger l'étiquette de test
+   img = charger_jpeg(CHEMIN_ETIQUETTE_TEST)
+
+   # La découper
+   caracteres = decouper(img)
+
+   # Visualiser le résultat du découpage
+   for caractere in caracteres:
+       afficher(caractere, "Test decouper etiquette", False)
 #
-#def test_visuel_etiquette_decouper():
-#    """
-#    Test visuel de la fonction de découpage.
-#    """
-#
-#    # Charger l'étiquette de test
-#    img = charger_jpeg(CHEMIN_ETIQUETTE_TEST)
-#
-#    # La découper
-#    caracteres = decouper(img)
-#
-#    # Visualiser le résultat du découpage
-#    for caractere in caracteres:
-#        afficher(caractere, "Test decouper etiquette", False)
-#    
