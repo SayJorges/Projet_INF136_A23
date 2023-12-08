70bb8d7986d97d1ffb9a8b05751dcccfc8253c53
Sayjorges <georges.sayegh.1@ens.etsmtl.ca>
2023-12-08 14:05:54 -0500

    sayjorges

--- rapport/rapport.py
+++ rapport/rapport.py
@@ -1,53 +1,53 @@
 
 
 def creer_dict():
+    etiquette = charger etiquette
+    dict ={}
+    dicti.append(etiquette)
 
-def remplir_dict():
+def nb_assy():
 
+    for i in dict:
+        for j in range(0,5)
+            if
 
-def ecrire():
-
-
-
-
-
-
-
-
+def nb_part():
 
 
 
 
 
-def generer(dict,):
 
-    DICTIO ={"01":"LAVAL","02":"LONGUEUIL","03":"ST-HUBERT","04":"KITCHENER","05":\
-        "CAMBRIDGE","06":"SPRINGFIELD","07":"WICHITA","08":"CLEVELAND","09":"NOTTINGHAM","10":"MADRID"}
 
+
+
+
+
+def generer():
 
     #ouvrir fichier text (fichier avec l'extension .txt) et le mode 'w' pour pouvoir écrire
-    with open(rapport_fichier, 'w') as fichier:
+    with open(generer, 'w') as fichier:
 
-        #initialiser compteur
-        c_laval_assy = 0
-        c_laval_part = 0
-
-
+        list_dict1 = nb_assy()
+        list_dict2 = nb_part()
         #écrire dans le rapport le nombre d'objet détectés à partir de la longueur de la liste
         fichier.write(f"Nombre d'objets détectés: {len(list_dict)}\n\n\n")
         #parcourir la liste qui les résultat de l'analyse rapport
-        for i in list_dict:
+        for i in list_dict1:
+            for j in list_dict2:
             #écrire dans le rapport avec ('text indique le résulat. {i pour nous situer [le résultat] 'retour')
             #le format utiliser pour fichier.write provient des notes de cours du cours 9 et chatgpt
-            fichier.write(f"Usine. {t}\n\n")
-            fichier.write(f"ASSY: {i['coords']}\n")
-            fichier.write(f"PART: {i['x_min']}\n")
-            fichier.write(f"TOTAL: {i['x_max']}\n")
-            fichier.write(f"y_min: {i['y_min']}\n")
-            fichier.write(f"y_max: {i['y_max']}\n")
-            fichier.write(f"hauteur: {i['hauteur']}\n")
-            fichier.write(f"largeur: {i['largeur']}\n")
-            fichier.write(f"angle: {i['angle']}\n\n")
+            fichier.write(f"Usine\t \t \t ASSY\t\t\t PART\t\t\t TOTAL\n\n")
+            fichier.write(f"Laval: {i['Laval_assy']}{j['Laval_part']}\n")
+            fichier.write(f"Longeuil: {i['Longeuil_assy']}{j['Longeuil_part']}\n")
+            fichier.write(f"St-Hubert: {i['St-Hubert_assy']}{j['St-Huber_part']}\n")
+            fichier.write(f"Kitchener: {i['Kitchener_assy']}{j['Kitchener_part']}\n")
+            fichier.write(f"Cambridge: {i['Cambridge_assy']}{j['Cambridge_part']}\n")
+            fichier.write(f"Springfield: {i['Springfield_assy']}{j['Springfield_part']}\n")
+            fichier.write(f"Witchita: {i['Witchita_assy']}{j['Witchita_part']}\n")
+            fichier.write(f"Cleveland: {i['Cleveland_assy']}{j['Cleveland_part']}\n")
+            fichier.write(f"Nottingham: {i['Nottingham_assy']}{j['Nottingham_part']}\n")
+            fichier.write(f"Madrid: {i['Madrid_assy']}{j['Madrid_part']}\n")
             # Ajout +1 au compteur pour différencier les objets
             nb_objet += 1
 
